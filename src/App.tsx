c3023259f1998b93a039616a738fb023875267c7
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-08-11 09:41:22 -0300

    eslint  config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <div className="App">
-      
       <h1>Vite + React</h1>
- 
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
