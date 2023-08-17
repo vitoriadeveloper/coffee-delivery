67a7dfa81735f9a3f855e65221a8a903262f2c10
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-08-17 19:43:16 -0300

    home finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,67 +1,10 @@
-import bigCoffee from "../../assets/Imagem.png";
-import blur from "../../assets/icons/blur.svg";
-import { ShoppingCart, Package, Timer, Coffee } from "phosphor-react";
-import {
-  Blur,
-  HomeContainer,
-  HomeContentColumn,
-  SectionContentRow,
-  Content,
-  SpanCart,
-  SpanCoffee,
-  SpanPackge,
-  SpanTimer,
-} from "./styles";
+import { Coffees } from "../../components/Coffee";
+import { Main } from "../../components/Main";
 export function Home() {
   return (
-    <HomeContainer>
-      <HomeContentColumn>
-        <Blur src={blur} alt="" />
-        <h1>
-          Encontre o café perfeito <br />
-          para qualquer hora do dia
-        </h1>
-
-        <p>
-          Com o Coffee Delivery você recebe seu café onde estiver, a <br />
-          qualquer hora
-        </p>
-
-        <SectionContentRow>
-          <Content>
-            <span>
-              <SpanCart>
-                <ShoppingCart size={16} weight="fill" color="white" />
-              </SpanCart>
-              Compra simples e segura
-            </span>
-            <span>
-              <SpanTimer>
-                <Timer size={16} weight="fill" color="white" />
-              </SpanTimer>
-              Entrega rápida e rastreada
-            </span>
-          </Content>
-          <Content>
-            <Blur src={blur} alt="" />
-            <span>
-              <SpanPackge>
-                <Package size={16} weight="fill" color="white" />
-              </SpanPackge>
-              Embalagem mantém o café intacto
-            </span>
-            <span>
-              <SpanCoffee>
-                <Coffee size={16} weight="fill" color="white" />
-              </SpanCoffee>
-              O café chega fresquinho até você
-            </span>
-          </Content>
-        </SectionContentRow>
-      </HomeContentColumn>
-      <Blur src={blur} alt="" />
-      <img src={bigCoffee} alt="" />
-      <Blur src={blur} alt="" />
-    </HomeContainer>
+    <>
+      <Main />
+      <Coffees />
+    </>
   );
 }
